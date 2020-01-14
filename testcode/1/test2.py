f240a08a91e5177e3efa0fbec2cc1f2fcb2d4e8d
hxj <huxiaojunchn@163.com>
2020-01-14 15:41:14 +0800

    测试git提交1

---
+++ testcode/1/test2.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+#-*- coding:utf-8 -*-
+#author:HXJ
+print("哈哈1")
+print("哈哈2")
+#TODO 记录将要做的事情
+print("哈哈3")
+a="12312313"
+def f():
+    for i in range(10):
+        print(i)
+
+f()
