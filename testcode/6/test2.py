13dbbeef9f41d8df09aaf465dd916adcbf6eaf5d
hxj <huxiaojunchn@163.com>
2020-02-01 21:16:05 +0800

    完成前三章

---
+++ testcode/6/test2.py
@@ -0,0 +1,79 @@
+user_0 = {
+'username': 'efermi',
+'first': 'enrico',
+'last': 'fermi',
+}
+for k, v in user_0.items():
+    print("\nKey: " + k)
+    print("Value: " + v)
+
+favorite_languages = {
+'jen': 'python',
+'sarah': 'c',
+'edward': 'ruby',
+'phil': 'python',
+}
+for name, language in favorite_languages.items():
+    print(name.title() + "'s favorite language is " +
+    language.title() + ".")
+
+favorite_languages = {
+'jen': 'python',
+'sarah': 'c',
+'edward': 'ruby',
+'phil': 'python',
+}
+for name in favorite_languages.keys():
+    print(name.title())
+
+favorite_languages = {
+'jen': 'python',
+'sarah': 'c',
+'edward': 'ruby',
+'phil': 'python',
+}
+friends = ['phil', 'sarah']
+for name in favorite_languages.keys():
+    print(name.title())
+    if name in friends:
+        print(" Hi " + name.title() +
+        ", I see your favorite language is " +
+        favorite_languages[name].title() + "!")
+
+favorite_languages = {
+'jen': 'python',
+'sarah': 'c',
+'edward': 'ruby',
+'phil': 'python',
+}
+if 'erin' not in favorite_languages.keys():
+    print("Erin, please take our poll!")
+
+favorite_languages = {
+'jen': 'python',
+'sarah': 'c',
+'edward': 'ruby',
+'phil': 'python',
+}
+for name in sorted(favorite_languages.keys()):
+    print(name.title() + ", thank you for taking the poll.")
+
+favorite_languages = {
+'jen': 'python',
+'sarah': 'c',
+'edward': 'ruby',
+'phil': 'python',
+}
+print("The following languages have been mentioned:")
+for language in favorite_languages.values():
+    print(language.title())
+#为剔除重复项，可使用集合（ set）。
+favorite_languages = {
+'jen': 'python',
+'sarah': 'c',
+'edward': 'ruby',
+'phil': 'python',
+}
+print("The following languages have been mentioned:")
+for language in set(favorite_languages.values()):
+    print(language.title())
---
+++ testcode/6/test3.py
@@ -0,0 +1,69 @@
+# 创建一个用于存储外星人的空列表
+aliens = []
+# 创建30个绿色的外星人
+for alien_number in range(30):
+    new_alien = {'color': 'green', 'points': 5, 'speed': 'slow'}
+    aliens.append(new_alien)
+# 显示前五个外星人
+for alien in aliens[:5]:
+    print(alien)
+print("...")
+# 显示创建了多少个外星人
+print("Total number of aliens: " + str(len(aliens)))
+
+# 创建一个用于存储外星人的空列表
+aliens = []
+# 创建30个绿色的外星人
+for alien_number in range (0,30):
+    new_alien = {'color': 'green', 'points': 5, 'speed': 'slow'}
+    aliens.append(new_alien)
+for alien in aliens[0:3]:
+    if alien['color'] == 'green':
+        alien['color'] = 'yellow'
+        alien['speed'] = 'medium'
+        alien['points'] = 10
+# 显示前五个外星人
+for alien in aliens[0:5]:
+    print(alien)
+print("...")
+
+# 存储所点比萨的信息
+pizza = {
+'crust': 'thick',
+'toppings': ['mushrooms', 'extra cheese'],
+}
+# 概述所点的比萨
+print("You ordered a " + pizza['crust'] + "-crust pizza " +
+"with the following toppings:")
+for topping in pizza['toppings']:
+    print("\t" + topping)
+
+favorite_languages = {
+'jen': ['python', 'ruby'],
+'sarah': ['c'],
+'edward': ['ruby', 'go'],
+'phil': ['python', 'haskell'],
+}
+for name, languages in favorite_languages.items():
+    print("\n" + name.title() + "'s favorite languages are:")
+    for language in languages:
+        print("\t" + language.title())
+
+users = {
+'aeinstein': {
+'first': 'albert',
+'last': 'einstein',
+'location': 'princeton',
+},
+'mcurie': {
+'first': 'marie',
+'last': 'curie',
+'location': 'paris',
+},
+}
+for username, user_info in users.items():
+    print("\nUsername: " + username)
+    full_name = user_info['first'] + " " + user_info['last']
+    location = user_info['location']
+    print("\tFull name: " + full_name.title())
+    print("\tLocation: " + location.title())
